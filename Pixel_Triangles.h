881be68ef28e5c9b9e675d16384d9ff24fa7ef27
Craig J Copi <cjc5@cwru.edu>
2011-08-09 21:10:09 +0000

    Refactor triangle code to handle more data/be more generic/less code duplication.  Quad info still here and extended but it will be moved.

--- Pixel_Triangles.h
+++ Pixel_Triangles.h
@@ -5,12 +5,15 @@
 #include <string>
 
 #include <healpix_base.h>
+#include <healpix_tables.h>
 #include <vec3.h>
+
+#include <Npoint_Functions_Utils.h>
 
 namespace {
   /// @cond IDTAG
   const std::string PIXEL_TRIANGLES_RCSID
-  ("$Id: Pixel_Triangles.h,v 1.20 2011-07-27 04:16:53 copi Exp $");
+  ("$Id: Pixel_Triangles.h,v 1.21 2011-07-27 19:50:00 copi Exp $");
   /// @endcond
 }
 
@@ -87,10 +90,15 @@ namespace Npoint_Functions {
   private :
     std::vector<std::vector<T> > triangles; // List of pixels in triangle.
     std::vector<double> edge_length; // Length of triangle edges.
-  protected :
      /// Orientation of the triangles.
     std::vector<Orientation> orient;
-
+    // Vectors to the center of HEALPix pixels.
+    std::vector<vec3> v;
+    // HEALPix Nside of the pixels in the triangles.
+    size_t nside;
+    // HEALPix ordering scheme for the pixels in the triangles.
+    Healpix_Ordering_Scheme scheme;
+  protected :
     /// Add a triangle to the list.
     inline void add (const T& p1, const T& p2, const T& p3)
     {
@@ -99,6 +107,8 @@ namespace Npoint_Functions {
       triangles[n][0] = p1;
       triangles[n][1] = p2;
       triangles[n][2] = p3;
+      orient.push_back (calculate_orientation (v[p1], v[p2],
+					       v[p3]));
     }
 
     /// Set the edge lengths of the triangle
@@ -109,15 +119,36 @@ namespace Npoint_Functions {
       this->edge_length[2] = l3;
     }
 
+
+    /** Internal routine for initializing the state of the class for a set
+     *  of two point tables.  The list of vectors to the HEALPix pixel
+     *  centers is also calculated.
+     */
+    void initialize (const Twopt_Table<T>& t1,
+		     const Twopt_Table<T>& t2,
+		     const Twopt_Table<T>& t3)
+    {
+      this->reset();
+      set_edge_lengths (t1.bin_value(), t2.bin_value(), t3.bin_value());
+      this->nside = t1.Nside();
+      this->scheme = t1.Scheme();
+      fill_vector_list (t1.Nside(), t1.Scheme(), v);
+    }
+
   public :
     /// Generic constructor.
-    Pixel_Triangles () : triangles(), edge_length(3), orient() {}
+    Pixel_Triangles () : triangles(), edge_length(3), orient(), v(),
+			 nside(0), scheme(NEST) {}
 
     /** Reset the list of triangles.
      *  All triangles are erased.
      */
     inline void reset()
-    { triangles.clear(); }
+    { 
+      triangles.clear();
+      v.clear();
+      orient.clear();
+    }
 
     /** Find all triangles.
      *  Find all the triangles that can be made up from the provided two
@@ -133,14 +164,8 @@ namespace Npoint_Functions {
       T p1, p2;
       T i2;
       std::vector<T> trip;
-      this->reset();
-      set_edge_lengths (t1.bin_value(), t2.bin_value(), t3.bin_value());
 
-
-      // Create list of vectors so we can get the orientation.
-      std::vector<vec3> v;
-      fill_vector_list (t1, v);
-      orient.clear();
+      this->initialize (t1, t2, t3);
 
       for (size_t i1=0; i1 < t1.Npix(); ++i1) {
 	p1 = t1.pixel_list(i1);
@@ -151,11 +176,9 @@ namespace Npoint_Functions {
 	  trip.clear();
 	  append_matches (&t2(i1,0), &t2(i1,t2.Nmax()),
 			  &t3(i2,0), &t3(i2,t3.Nmax()), trip);
-	  // Now put all the triplets in the list
+	  // Now put all the triplets in the list.
 	  for (size_t k=0; k < trip.size(); ++k) {
 	    add (p1, p2, t1.pixel_list(trip[k]));
-	    orient.push_back (calculate_orientation (v[i1], v[i2],
-						     v[trip[k]])); 
 	  }
 	}
       }
@@ -194,6 +217,12 @@ namespace Npoint_Functions {
      */
     inline const std::vector<double>& lengths() const
     { return edge_length; }
+    /** Nside of the pixels in the triangles.
+     */
+    inline int Nside() const { return nside; }
+    /** HEALPix ordering scheme of the pixels in the triangles.
+     */
+    inline Healpix_Ordering_Scheme Scheme() const { return scheme; }
     //@}
   };
 
@@ -201,9 +230,7 @@ namespace Npoint_Functions {
   /** Storage for isosceles pixel triangles.
    *  Only the unique triangles are stored.  The angular distance between
    *  pixel pairs 2,3 and 3,1 are equal.  The angular distance between pixel
-   *  pair 1,2 is different than the other two pairs.  The order of pixels
-   *  1 and 2 is set to ensure that the orientation of the triangle is
-   *  righthanded. 
+   *  pair 1,2 is different than the other two pairs.
    *
    *  This is a specialized version of Pixel_Triangles.
    */
@@ -225,13 +252,8 @@ namespace Npoint_Functions {
       T p1, p2;
       T i2;
       std::vector<T> trip;
-      this->reset();
-      set_edge_lengths (tother.bin_value(), tequal.bin_value(),
-			tequal.bin_value());
 
-      // Create list of vectors so we can get the orientation.
-      std::vector<vec3> v;
-      fill_vector_list (tequal, v);
+      this->initialize (tother, tequal, tequal);
 
       for (size_t i1=0; i1 < tother.Npix(); ++i1) {
 	p1 = tother.pixel_list(i1);
@@ -244,28 +266,19 @@ namespace Npoint_Functions {
 	  trip.clear();
 	  append_matches (&tequal(i1,0), &tequal(i1,tequal.Nmax()),
 			  &tequal(i2,0), &tequal(i2,tequal.Nmax()), trip);
-	  /* Now put all the triplets in the list making sure the triangles
-	   * are righthanded.  The first two pixels are swapped if the
-	   * given orientation is lefthanded.
-	   */
+	  // Now put all the triplets in the list.
 	  for (size_t k=0; k < trip.size(); ++k) {
-	    if (calculate_orientation (v[i1], v[i2], v[trip[k]]) 
-		== RIGHTHANDED)
-	      add (p1, p2, tequal.pixel_list(trip[k]));
-	    else
-	      add (p2, p1, tequal.pixel_list(trip[k]));
+	    add (p1, p2, tequal.pixel_list(trip[k]));
 	  }
 	}
       }
-      // All triangles are righthanded.
-      this->orient.assign (this->size(), RIGHTHANDED);
     }
   };
 
   /** Storage for equilateral pixel triangles.
-   *  Only the unique triangles are stored.  The pixels are stored as to
-   *  ensure that the triangles are righthanded and that the first pixel
-   *  has the smallest index.
+   *  Only the unique triangles are stored.  The pixels are stored as in
+   *  monotonically increasing order.  This fact can be used to speed up
+   *  searches through the triangles.
    *
    *  This is a specialized version of Pixel_Triangles_Isosceles.
    */
@@ -285,12 +298,8 @@ namespace Npoint_Functions {
       T p1, p2;
       T i2;
       std::vector<T> trip;
-      this->reset();
-      set_edge_lengths (t.bin_value(), t.bin_value(), t.bin_value());
 
-      // Create list of vectors so we can get the orientation.
-      std::vector<vec3> v;
-      fill_vector_list (t, v);
+      this->initialize(t, t, t);
 
       for (size_t i1=0; i1 < t.Npix(); ++i1) {
 	p1 = t.pixel_list(i1);
@@ -302,26 +311,17 @@ namespace Npoint_Functions {
 	  trip.clear();
 	  append_matches (i2, &t(i1,0), &t(i1,t.Nmax()),
 			  &t(i2,0), &t(i2,t.Nmax()), trip);
-	  /* Now put all the triplets in the list making sure the triangles
-	   * are righthanded.  The last two pixels are swapped if the
-	   * given orientation is lefthanded.
-	   */
+	  // Now put all the triplets in the list.
 	  for (size_t k=0; k < trip.size(); ++k) {
-	    if (calculate_orientation (v[i1], v[i2], v[trip[k]]) 
-		== RIGHTHANDED)
-	      add (p1, p2, t.pixel_list(trip[k]));
-	    else
-	      add (p1, t.pixel_list(trip[k]), p2);
+	    add (p1, p2, t.pixel_list(trip[k]));
 	  }
 	}
       }
-      // All triangles are righthanded.
-      this->orient.assign (this->size(), RIGHTHANDED);
     }
   };
 
   // Temporary location?
-  /** Calculate all quadrilaterals.  This is specialized to Isosceles
+  /** Calculate all quadrilaterals.  This is specialized to Equilateral
    * triangles and only calculates equilateral quadrilaterals.  We use the
    * fact that the pixels in the triangle are stored such that the triangles
    * are righthanded.  We further use the fact that min(pix1, pix2) will
@@ -335,33 +335,184 @@ namespace Npoint_Functions {
   class Quads {
   private :
     size_t ind_curr;
-    Pixel_Triangles_Isosceles<T> *t;
+    Pixel_Triangles_Equilateral<T> *t;
     std::vector<T> pts; // So we don't have to keep recreating it.
   public :  
     Quads () : ind_curr(0), t(0), pts(4) {}
-    void initialize (Pixel_Triangles_Isosceles<T>& triangle)
+    void initialize (Pixel_Triangles_Equilateral<T>& triangle)
     { ind_curr = 0; t = &triangle; }
     bool next (std::vector<std::vector<T> >& quads)
     {
       if (ind_curr == t->size()) return false;
       quads.clear();
-      T min1, min2;
       // Order of pts doesn't matter.
-      std::copy (t->get(ind_curr).begin(), t->get(ind_curr).end(), pts.begin());
-      min1 = std::min (t->get(ind_curr,0), t->get(ind_curr,1));
+      std::copy (t->get(ind_curr).begin(), t->get(ind_curr).end(),
+		 pts.begin());
+      T cmax = std::max (pts[1], pts[2]);
       for (size_t jj=ind_curr+1; jj < t->size(); ++jj) {
-	min2 = std::min (t->get(jj,0), t->get(jj,1));
-	if (min2 > min1) break;
-	if (
-	    ((t->get(ind_curr,0) == t->get(jj,1))
-	     && (t->get(ind_curr,1) == t->get(jj,0)))
-	    // Allow lines to cross!
-	    || ((t->get(ind_curr,0) == t->get(jj,0))
-		&& (t->get(ind_curr,1) == t->get(jj,1)))
-	    ) {
-	  pts[3] = t->get(jj,2);
-	  quads.push_back (pts);
+	if (t->get(jj,0) > cmax) break;
+	if (t->get(jj,0) == pts[0]) {
+	  if (pts[1] == t->get(jj,2)) {
+	    pts[3] = t->get(jj,1);
+	  } else if (pts[2] == t->get(jj,1)) {
+	    pts[3] = t->get(jj,2);
+	  } else {
+	    continue;
+	  }
+	} else if (pts[1] == t->get(jj,0)) {
+	  if (pts[2] == t->get(jj,2)) {
+	    pts[3] = t->get(jj,1);
+	  } else {
+	    continue;
+	  }
+	} else if (pts[1] == t->get(jj,1)) {
+	  if (pts[2] == t->get(jj,0)) {
+	    pts[3] = t->get(jj,2);
+	  } else {
+	    continue;
+	  }
+	} else if (pts[1] == t->get(jj,2)) {
+	  if (pts[2] == t->get(jj,1)) {
+	    pts[3] = t->get(jj,0);
+	  } else {
+	    continue;
+	  }
+	} else {
+	  continue;
 	}
+	quads.push_back (pts);
+      }
+      ++ind_curr;
+      return true;
+    }
+  };
+
+
+  /** Calculate all rhombic quadrilaterals.  Rhombic quadrilaterals are
+   *  constructed from two equilateral triangles connected along one side,
+   *  that is, all sides have the same length and one of the diagonals has
+   *  the length of the sides.  We use the fact that the pixels in the
+   *  triangle are stored in monotonically increasing order.
+   *
+   *  Even with this specialization the quad table can be huge.  For this
+   *  reason we create a class that incrementally calculates sets of points.
+   *  This costs more in overhead but requires significantly less memory.
+   */
+  template<typename T>
+  class Quadrilaterals_Rhombic {
+  private :
+    size_t ind_curr;
+    Pixel_Triangles_Equilateral<T> *t;
+  public :  
+    Quadrilaterals_Rhombic () : ind_curr(0), t(0) {}
+    /** Initialize the rhombic quadrilateral search.
+     *  The given triangle will be used for subsequent searches.  See
+     *  next().
+     */
+    void initialize (Pixel_Triangles_Equilateral<T>& triangle)
+    { 
+      ind_curr = 0; t = &triangle;
+    }
+    /** Get the next set of rhombic quadrilaterals.
+     *  The quadrilaterals are constructed for each triangle provided to
+     *  initialize(). The quadrilaterals are then made up of the three
+     *  points in the triangle, returned in \a pts, and a third
+     *  point.  The list of all third points is returned in \a thirdpt.
+     *  Note that the orientation of the quadrilateral is lost in this
+     *  process.  We do \b not ensure that the quadrilaterals are right
+     *  handed.
+     */
+    bool next (std::vector<T>& pts, std::vector<T>& thirdpt)
+    {
+      if (ind_curr >= t->size()-1) return false;
+      thirdpt.clear();
+      pts.resize(3);
+      // Points are not ordered in any special way.
+      std::copy (t->get(ind_curr).begin(), t->get(ind_curr).end(),
+		 pts.begin());
+      // Shorthand
+      Npoint_Functions::Orientation o = t->orientation(ind_curr);
+      size_t j = ind_curr+1;
+      // First loop over triangles with the first two points equal
+      while ((j < t->size())
+	     && (t->get(j,1) == pts[1])
+	     && (t->get(j,0) == pts[0])) {
+	if (o != t->orientation(j)) {
+	  thirdpt.push_back (t->get(j,2));
+	}
+	++j;
+      }
+      /* Next loop over triangles looking for the case when the first and
+       * third points are equal to the first and third points of our base
+       * triangle. */
+      while ((j < t->size())
+	     && (t->get(j,1) < pts[2])
+	     && (t->get(j,0) == pts[0])) {
+	if ((o != t->orientation(j)) && (t->get(j,2) == pts[2])) {
+	  thirdpt.push_back (t->get(j,1));
+	}
+	++j;
+      }
+      /* Next skip to triangles where the first and third points are equal
+       * to the first and second points of our base triangle. */
+      while ((j < t->size())
+	     && (t->get(j,1) < pts[2])
+	     && (t->get(j,0) == pts[0])) ++j;
+      // Now loop over these triangles.
+      while ((j < t->size())
+	     && (t->get(j,1) == pts[2])
+	     && (t->get(j,0) == pts[0])) {
+	if (o == t->orientation(j)) {
+	  thirdpt.push_back (t->get(j,2));
+	}
+	++j;
+      }
+      /* Next look for triangles with the second and third points equal to
+       * the second and third points of our base triangle. Unfortunately I
+       * don't know of a smarter way to do this. */
+      T prev;
+      while ((j < t->size()) && (t->get(j,0) < pts[1])) {
+	prev = t->get(j,0);
+	/* For the given value of the first point skip triangles until we
+	 * get to one where the second point can possibly match the second
+	 * point of the base triangle. */
+	while ((j < t->size()) 
+	       && (t->get(j,1) < pts[1])
+	       && (t->get(j,0) == prev)) ++j;
+	/* Now loop over the triangles where the second point matches. */
+	while ((j < t->size()) && (t->get(j,1) == pts[1])
+	       && (t->get(j,0) == prev)) {
+	  if ((o != t->orientation(j)) && 
+	      (t->get(j,2) == pts[2])) {
+	    thirdpt.push_back(t->get(j,0));
+	  }
+	  ++j;
+	}
+	/* Now skip the rest of the triangles where the first point
+	 * matches. */
+	while ((j < t->size()) && (t->get(j,0) == prev)) ++j;
+      }
+      /* Now loop over the triangles while its second point is less
+       * than the third point of our base triangle. Look for triangles in
+       * which the third point equals the third point of our base
+       * triangle. */
+      while ((j < t->size()) 
+	     && (t->get(j,1) < pts[2])
+	     && (t->get(j,0) == pts[1])) {
+	if ((o == t->orientation(j)) && (t->get(j,2) == pts[2])) {
+	  thirdpt.push_back (t->get(j,1));
+	}
+	++j;
+      }
+      /* Finally loop over the triangles where the first and second points
+       * are equal to the second and third points of our base triangle. */
+      while ((j < t->size()) 
+	     && (t->get(j,1) == pts[2])
+	     && (t->get(j,0) == pts[1])) {
+	if (o != t->orientation(j)) {
+	  thirdpt.push_back (t->get(j,2));
+	}
+	++j;
       }
       ++ind_curr;
       return true;
